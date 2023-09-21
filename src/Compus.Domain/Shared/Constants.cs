1ee0c09a7094a0ddb5d6ba8b1460f5bad423f5b2
sh1ngekyo <repllimate@gmail.com>
2023-09-21 09:09:12 +0300

    Add shared constants

---
+++ src/Compus.Domain/Shared/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace Compus.Domain.Shared
+{
+    public static class Constants
+    {
+        public const string ClientSessionIdName = "ClientSessionId";
+
+        public const int MaxinumLines = 1000;
+
+        public const int MaxinumQueueCount = 10000;
+
+        public const int MaxinumOutputLength = 100000;
+
+        public const string NewLineForShell = "\r\n";
+    }
+}
