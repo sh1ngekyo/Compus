679d6071888841c6351373e907d165123048c0be
sh1ngekyo <repllimate@gmail.com>
2023-09-21 12:28:12 +0300

    Implement simple captcha generation

---
+++ tests/Compus.Tests/Application/Captcha/GenerateTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Compus.Application.Abstractions;
+using Compus.Application.Services.Captcha;
+using SixLabors.ImageSharp;
+
+namespace Compus.Tests.Application.Captcha;
+public class GenerateTest
+{
+    [Fact]
+    public void GenerateCaptcha_LengthShouldBeEqualsServiceLength()
+    {
+        var captchaService = new CaptchaService();
+
+        var captcha = captchaService.Generate(200, 60, out var image);
+
+        Assert.Equal(CaptchaService.CaptchaLength, captcha.Length);
+    }
+
+    [Fact]
+    public void GenerateCaptcha_ShouldContainsOnlyAlphaNumericChars()
+    {
+        var captchaService = new CaptchaService();
+
+        var captcha = captchaService.Generate(200, 60, out var image);
+
+        foreach (var ch in captcha)
+            Assert.Contains(ch, captchaService.Characters);
+    }
+
+    [Fact]
+    public void GenerateCaptcha_ImageSizeShouldBeEqualsExpectedSize()
+    {
+        var width = 200;
+        var height = 60;
+
+        var captchaService = new CaptchaService();
+
+        var captcha = captchaService.Generate(width, height, out var rawImage);
+
+        using var ms = new MemoryStream(rawImage);
+        var image = Image.Load(ms);
+        Assert.Equal(width, image.Width);
+        Assert.Equal(height, image.Height);
+    }
+}
